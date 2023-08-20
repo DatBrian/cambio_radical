890b8b73ae6f10bc04d9bf9185ce71de25151ae4
DatBrian <vikard.2005@hotmail.com>
2023-08-20 17:42:27 -0500

    feat: :sparkles: Nueva opcion en comunas
    Se agregó la comuna 0

--- src/components/VotanteForm.vue
+++ src/components/VotanteForm.vue
@@ -79,6 +79,7 @@
         @change="handleComunaChange"
       >
         <option disabled value="">Selecciona una comuna</option>
+        <option value="0">0</option>
         <option value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -327,7 +328,7 @@ const saveVotante = async () => {
             MesaVotacion: "",
             compromiso: "",
             fidelidad: "",
-            observaciones: ""
+            observaciones: "",
           };
           redesSociales.value = [{ red: "", usuario: "" }];
         } else {
